40046f7e57229ec2252d11252783bcb8acc30718
dmytro <karmeljuk@gmail.com>
2020-12-23 12:02:41 +0200

    Trello #28: Clean php files

--- wp-content/themes/brooklyn-child/woocommerce/single-product/up-sells.php
+++ wp-content/themes/brooklyn-child/woocommerce/single-product/up-sells.php
@@ -20,29 +20,4 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-//if ( $upsells ) : ?>
-
-	<!-- <section class="up-sells upsells products">
-
-		<h2><?php //esc_html_e( 'You may also like&hellip;', 'woocommerce' ) ?></h2>
-
-		<?php //woocommerce_product_loop_start(); ?>
-
-			<?php //foreach ( $upsells as $upsell ) : ?>
-
-				<?php
-				 	//$post_object = get_post( $upsell->get_id() );
-
-					//setup_postdata( $GLOBALS['post'] =& $post_object );
-
-					//wc_get_template_part( 'content', 'product' ); ?>
-
-			<?php //endforeach; ?>
-
-		<?php//woocommerce_product_loop_end(); ?>
-
-	</section> -->
-
-<?php //endif;
-
 wp_reset_postdata();
