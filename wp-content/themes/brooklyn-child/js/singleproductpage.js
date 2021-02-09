a4f2b20e0c90dd2a8b922bd979547ba742bc575b
dmytro <karmeljuk@gmail.com>
2021-02-09 18:02:06 +0200

    Trello #133: Iterate through each pricing section

--- wp-content/themes/brooklyn-child/js/singleproductpage.js
+++ wp-content/themes/brooklyn-child/js/singleproductpage.js
@@ -1,11 +1,16 @@
 	jQuery(document).ready(function ($) {
-		var variationSections = [
-			'.variations_form .p0',
-			'.variations_form .p1',
-			'.variations_form .p2'
-		]
+		var variationSections = []
 		var variationInputClass = '.variation_price';
 		var dataVariationPrice = 'data-variation-price';
+
+		$('.variations .value').each(function(index) {
+			console.log(this)
+			variationSections[index] = '#' + this.id + ' ' + variationInputClass;
+		});
+
+		// console.log('variationSections = ', variationSections);
+
+		addPriceToAllVariations(variationSections);
 
 		// add currency symbol and plus or minus
 		function getPriceWithSymbol(number, symbol) {
@@ -13,15 +18,54 @@
 		}
 
 		function addPriceToAllVariations(selectorArray) {
+			var combos = [];
+			
+			// $(selectorArray).each(function(index, element) {
+
+			// })
+
 			//ToDo: optimzie array
-			addPriceToVariation(selectorArray[0] + ' ' + variationInputClass, selectorArray[1] + ' ' + variationInputClass + ':checked', selectorArray[2] + ' ' + variationInputClass + ':checked');
-			addPriceToVariation(selectorArray[1] + ' ' + variationInputClass, selectorArray[2] + ' ' + variationInputClass + ':checked', selectorArray[0] + ' ' + variationInputClass + ':checked');
-			addPriceToVariation(selectorArray[2] + ' ' + variationInputClass, selectorArray[0] + ' ' + variationInputClass + ':checked', selectorArray[1] + ' ' + variationInputClass + ':checked');
+			// addPriceToVariation(selectorArray[0], selectorArray[1] + ':checked', selectorArray[2] + ':checked');
+			// addPriceToVariation(selectorArray[1], selectorArray[2] + ':checked', selectorArray[0] + ':checked');
+			// addPriceToVariation(selectorArray[2], selectorArray[0] + ':checked', selectorArray[1] + ':checked');
+
+
+			var firstArrayCopy = Array.from(selectorArray);
+			var secondArray = firstArrayCopy.concat(firstArrayCopy.splice(0, 1));
+			var secondArrayCopy = Array.from(secondArray);
+			var thirdArray = secondArrayCopy.concat(secondArrayCopy.splice(0, 1));
+
+			console.log('thirdArray = ', thirdArray);
+			console.log('secondArray = ', secondArray);
+			console.log('selectorArray = ', selectorArray);
+
+			// ToDo: fix hardcoded condition
+			if (selectorArray.length === 2) {
+				for (var i = 0; i < selectorArray.length; i++) {
+				
+					combos.push(selectorArray[i] + ',' + secondArray[i] + ':checked');
+					addPriceToVariation(selectorArray[i], secondArray[i] + ':checked');
+				}
+			} 
+
+			if (selectorArray.length === 3) {
+				for (var i = 0; i < selectorArray.length; i++) {
+				
+					combos.push(selectorArray[i] + ',' + secondArray[i] + ':checked' + ',' + thirdArray[i] + ':checked');
+					addPriceToVariation(selectorArray[i], secondArray[i] + ':checked', thirdArray[i] + ':checked');
+				}
+			} 
+
+			console.log('combos = ', combos);
 		}
 
 		// create array with writer level and interview format
 		function createCombination(variant, firstSelector, secondSelector) {
 			var compare = [];
+			// console.log('variant = ', variant);
+			// console.log('firstSelector = ', firstSelector);
+			// console.log('secondSelector = ', secondSelector);
+
 			compare.push($(variant).val(), $(firstSelector).val(), $(secondSelector).val());	
 
 			console.log('compare = ', compare);
@@ -30,7 +74,13 @@
 
 		// add price to the variation data-variation-price 
 		function addPriceToVariation(currentVariation, secondVariation, thirdVariation) {
+			// console.log('currentVariation = ', currentVariation);
+			// console.log('secondVariation = ', secondVariation);
+			// console.log('thirdVariation = ', thirdVariation);
+
 			$(currentVariation).each(function() {
+
+				// console.log('start each compare');
 				var result = filterFromData(createCombination(this, secondVariation, thirdVariation));
 				
 				$(this).attr(dataVariationPrice, result);
@@ -55,7 +105,7 @@
 				if (JSON.stringify(result.sort()) == JSON.stringify(compare.sort())) {	
 					// console.log(JSON.stringify(result.sort()));
 					// console.log(JSON.stringify(compare.sort()));
-					console.log('formData[i].display_price = ', formData[i].display_price);
+					// console.log('formData[i].display_price = ', formData[i].display_price);
 					return formData[i].display_price;
 				}	
 			}			
@@ -72,11 +122,15 @@
 				var passiveVariation = generalVaration.filter(':not(:checked)');
 
 				console.log('passiveVariation = ', passiveVariation);
+				console.log('activeVariation = ', activeVariation);
 
 				passiveVariation.each(function () {
 					var pricingDiff = $(this).attr(dataVariationPrice) - activeVariation.attr(dataVariationPrice);
 
 					console.log('$(this).parents()', $(this).closest('#pa_payment-plan').length);
+					// console.log('!!!!!!pricingDiff = ', pricingDiff);
+					// console.log('!!!!!!pricingDiff 1 = ', $(this).attr(dataVariationPrice));
+					// console.log('!!!!!!pricingDiff 2 = ', activeVariation.attr(dataVariationPrice));
 					//if ($(this).parents('#pa_payment-plan')) return;
 					
 					//dont add diff for payment plan section
@@ -92,26 +146,53 @@
 
 			// calculate diff on click to the variant
 			generalVaration.on('change', function () {
+				if ($(this).val() === 'single-payment' || $(this).val() === '10-deposit') return;
+
 				$(this).siblings('.variation-diff-price').html('');
 
 				console.log('$(this).siblings = ', $(this).siblings('.variation-diff-price'))
 				
 				addPriceToAllVariations(variationSections);
 
-				$.each(variationSections, function(...value) {
-					console.log('value = ', value[1]);
+				$('.variations .value').each(function() {
+					// console.log('value = ', $(this));
 			
-					findPricingDiff($(value[1]));
+					findPricingDiff($(this));
 				});
 			});
 		};
 
 		// run all variations functions
-		addPriceToAllVariations(variationSections);
+		//addPriceToAllVariations(variationSections);
 
-		$.each(variationSections, function(...value) {
-			calculatePricingDiff($(value[1]));
+		// var stuff = [];
+
+		$('.variations .value').each(function() {
+			calculatePricingDiff($(this));
+
+			// variationSections[index] = element;
+
+			//console.log('stuff = ', stuff)
+
+			//addPriceVariation($(this));
 		});
+
+		//addPriceToAllVariations(variationSections);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 		$(".btn-gotocart").click(function () {
 			$(".cartload").addClass('addgry');
