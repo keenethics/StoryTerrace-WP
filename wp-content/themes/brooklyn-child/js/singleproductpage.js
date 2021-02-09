443a23ec4a0fde31d60bf01b56a8d21a2362aea7
dmytro <karmeljuk@gmail.com>
2021-02-09 14:07:29 +0200

    Trello #133: Fix NL pricing with third section

--- wp-content/themes/brooklyn-child/js/singleproductpage.js
+++ wp-content/themes/brooklyn-child/js/singleproductpage.js
@@ -1,7 +1,8 @@
 	jQuery(document).ready(function ($) {
 		var variationSections = [
 			'.variations_form .p0',
-			'.variations_form .p1'
+			'.variations_form .p1',
+			'.variations_form .p2'
 		]
 		var variationInputClass = '.variation_price';
 		var dataVariationPrice = 'data-variation-price';
@@ -12,22 +13,25 @@
 		}
 
 		function addPriceToAllVariations(selectorArray) {
-			addPriceToVariation(selectorArray[0] + ' ' + variationInputClass, selectorArray[1] + ' ' + variationInputClass + ':checked');
-			addPriceToVariation(selectorArray[1] + ' ' + variationInputClass, selectorArray[0] + ' ' + variationInputClass + ':checked');
+			//ToDo: optimzie array
+			addPriceToVariation(selectorArray[0] + ' ' + variationInputClass, selectorArray[1] + ' ' + variationInputClass + ':checked', selectorArray[2] + ' ' + variationInputClass + ':checked');
+			addPriceToVariation(selectorArray[1] + ' ' + variationInputClass, selectorArray[2] + ' ' + variationInputClass + ':checked', selectorArray[0] + ' ' + variationInputClass + ':checked');
+			addPriceToVariation(selectorArray[2] + ' ' + variationInputClass, selectorArray[0] + ' ' + variationInputClass + ':checked', selectorArray[1] + ' ' + variationInputClass + ':checked');
 		}
 
 		// create array with writer level and interview format
-		function createCombination(variant, selector) {
+		function createCombination(variant, firstSelector, secondSelector) {
 			var compare = [];
-			compare.push($(variant).val(), $(selector).val());	
+			compare.push($(variant).val(), $(firstSelector).val(), $(secondSelector).val());	
 
-			return $(variant).parents('.p0').length ? compare : compare.reverse();
+			console.log('compare = ', compare);
+			return compare;
 		}
 
 		// add price to the variation data-variation-price 
-		function addPriceToVariation(currentVariation, oppositeVariation) {
+		function addPriceToVariation(currentVariation, secondVariation, thirdVariation) {
 			$(currentVariation).each(function() {
-				var result = filterFromData(createCombination(this, oppositeVariation));
+				var result = filterFromData(createCombination(this, secondVariation, thirdVariation));
 				
 				$(this).attr(dataVariationPrice, result);
 			})
@@ -35,17 +39,24 @@
 
 		// filter json data from woocommerce form
 		function filterFromData(compare) {
-			var formData = $('.variations_form').data( 'product_variations' );
+			var formData = $('.variations_form').data('product_variations');
 
 			for (var i=0; i < formData.length; i++) {
 				var a = formData[i].attributes;				
 				var result = Object.keys(a).map(function(key){
-					if (key !== 'attribute_pa_payment-plan') return a[key];									
+					// if (key !== 'attribute_pa_payment-plan') return a[key];	
+					return a[key];									
 				})			
 				
+
+
+
 				// slice relust array for GB and NL locale
-				if (JSON.stringify(result.slice(0, 2)) === JSON.stringify(compare)) {	
-					 return formData[i].display_price;
+				if (JSON.stringify(result.sort()) == JSON.stringify(compare.sort())) {	
+					// console.log(JSON.stringify(result.sort()));
+					// console.log(JSON.stringify(compare.sort()));
+					console.log('formData[i].display_price = ', formData[i].display_price);
+					return formData[i].display_price;
 				}	
 			}			
 		}
@@ -60,8 +71,17 @@
 				var activeVariation = generalVaration.filter(':checked');
 				var passiveVariation = generalVaration.filter(':not(:checked)');
 
+				console.log('passiveVariation = ', passiveVariation);
+
 				passiveVariation.each(function () {
 					var pricingDiff = $(this).attr(dataVariationPrice) - activeVariation.attr(dataVariationPrice);
+
+					console.log('$(this).parents()', $(this).closest('#pa_payment-plan').length);
+					//if ($(this).parents('#pa_payment-plan')) return;
+					
+					//dont add diff for payment plan section
+					if ($(this).closest('#pa_payment-plan').length) return;
+
 	
 					$(this).siblings('.variation-diff-price').html(getPriceWithSymbol(pricingDiff, shopSymbol));
 				})
@@ -73,10 +93,14 @@
 			// calculate diff on click to the variant
 			generalVaration.on('change', function () {
 				$(this).siblings('.variation-diff-price').html('');
+
+				console.log('$(this).siblings = ', $(this).siblings('.variation-diff-price'))
 				
 				addPriceToAllVariations(variationSections);
 
 				$.each(variationSections, function(...value) {
+					console.log('value = ', value[1]);
+			
 					findPricingDiff($(value[1]));
 				});
 			});
