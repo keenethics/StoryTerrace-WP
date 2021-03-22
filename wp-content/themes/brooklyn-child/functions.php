77e994f0a9851b095809fb831d5388c9c223ac6f
dmytro <karmeljuk@gmail.com>
2021-03-22 17:11:28 +0200

    Trello #142: Fix team page card excerpt text

--- wp-content/themes/brooklyn-child/functions.php
+++ wp-content/themes/brooklyn-child/functions.php
@@ -438,41 +438,121 @@ function convert_text_to_underscore($str, array $noStrip = []){
         return $str;
 }
 
+
 /**
- * Get The First Image From a Post
- * @link https://css-tricks.com/snippets/wordpress/get-the-first-image-from-a-post/
+ * truncateHtml can truncate a string up to a number of characters while preserving whole words and HTML tags
  *
- * @return string
+ * @param string $text String to truncate.
+ * @param integer $length Length of returned string, including ellipsis.
+ * @param string $ending Ending to be appended to the trimmed string.
+ * @param boolean $exact If false, $text will not be cut mid-word
+ * @param boolean $considerHtml If true, HTML tags would be handled correctly
+ *
+ * @return string Trimmed string.
  */
-function catch_first_image() {
-    global $post;
-    $first_img = '';
-    ob_start();
-    ob_end_clean();
-
-    $output = preg_match_all('/<img.+?src=[\'"]([^\'"]+)[\'"].*?>/i', $post->post_content, $matches);
-    $first_img = $matches[1][0];
-
-    if (empty($first_img)) {
-        $first_img = get_template_directory_uri() . "/images/placeholder/team-member.png";
+function truncateHtml($text, $length = 100, $ending = '...', $exact = false, $considerHtml = true) {
+    if ($considerHtml) {
+        // if the plain text is shorter than the maximum length, return the whole text
+        if (strlen(preg_replace('/<.*?>/', '', $text)) <= $length) {
+            return $text;
+        }
+        // splits all html-tags to scanable lines
+        preg_match_all('/(<.+?>)?([^<>]*)/s', $text, $lines, PREG_SET_ORDER);
+        $total_length = strlen($ending);
+        $open_tags = array();
+        $truncate = '';
+        foreach ($lines as $line_matchings) {
+            // if there is any html-tag in this line, handle it and add it (uncounted) to the output
+            if (!empty($line_matchings[1])) {
+                // if it's an "empty element" with or without xhtml-conform closing slash
+                if (preg_match('/^<(\s*.+?\/\s*|\s*(img|br|input|hr|area|base|basefont|col|frame|isindex|link|meta|param)(\s.+?)?)>$/is', $line_matchings[1])) {
+                    // do nothing
+                    // if tag is a closing tag
+                } else if (preg_match('/^<\s*\/([^\s]+?)\s*>$/s', $line_matchings[1], $tag_matchings)) {
+                    // delete tag from $open_tags list
+                    $pos = array_search($tag_matchings[1], $open_tags);
+                    if ($pos !== false) {
+                        unset($open_tags[$pos]);
+                    }
+                    // if tag is an opening tag
+                } else if (preg_match('/^<\s*([^\s>!]+).*?>$/s', $line_matchings[1], $tag_matchings)) {
+                    // add tag to the beginning of $open_tags list
+                    array_unshift($open_tags, strtolower($tag_matchings[1]));
+                }
+                // add html-tag to $truncate'd text
+                $truncate .= $line_matchings[1];
+            }
+            // calculate the length of the plain text part of the line; handle entities as one character
+            $content_length = strlen(preg_replace('/&[0-9a-z]{2,8};|&#[0-9]{1,7};|[0-9a-f]{1,6};/i', ' ', $line_matchings[2]));
+            if ($total_length + $content_length > $length) {
+                // the number of characters which are left
+                $left = $length - $total_length;
+                $entities_length = 0;
+                // search for html entities
+                if (preg_match_all('/&[0-9a-z]{2,8};|&#[0-9]{1,7};|[0-9a-f]{1,6};/i', $line_matchings[2], $entities, PREG_OFFSET_CAPTURE)) {
+                    // calculate the real length of all entities in the legal range
+                    foreach ($entities[0] as $entity) {
+                        if ($entity[1] + 1 - $entities_length <= $left) {
+                            $left--;
+                            $entities_length += strlen($entity[0]);
+                        } else {
+                            // no more characters left
+                            break;
+                        }
+                    }
+                }
+                $truncate .= substr($line_matchings[2], 0, $left + $entities_length);
+                // maximum lenght is reached, so get off the loop
+                break;
+            } else {
+                $truncate .= $line_matchings[2];
+                $total_length += $content_length;
+            }
+            // if the maximum length is reached, get off the loop
+            if ($total_length >= $length) {
+                break;
+            }
+        }
+    } else {
+        if (strlen($text) <= $length) {
+            return $text;
+        } else {
+            $truncate = substr($text, 0, $length - strlen($ending));
+        }
     }
-
-    return $first_img;
+    // if the words shouldn't be cut in the middle...
+    if (!$exact) {
+        // ...search the last occurance of a space...
+        $spacepos = strrpos($truncate, ' ');
+        if (isset($spacepos)) {
+            // ...and cut the text in this position
+            $truncate = substr($truncate, 0, $spacepos);
+        }
+    }
+    // add the defined ending to the text
+    $truncate .= $ending;
+    if ($considerHtml) {
+        // close all unclosed html-tags
+        foreach ($open_tags as $tag) {
+            $truncate .= '</' . $tag . '>';
+        }
+    }
+    return $truncate;
 }
 
 /**
- * Get The Team member role From a Post
+ * Get specific content for team page
  *
- * @return string
+ * @param integer $length Length of returned string, including ellipsis.
+ *
+ * @return string Team card cotnent
  */
-function catch_team_member_role() {
-    global $post;
-    $team_member_role = '';
-    ob_start();
-    ob_end_clean();
+function get_team_content($length) {
+    $content = get_the_content();
+    $content = apply_filters('the_content', $content);
+    $content = preg_replace('~<style(.*?)</style>~Usi', "", $content); //remove <style> tag
+    $content = preg_replace('#<div[^>]*class[^>]*>#is', '<div>', $content); //remove all classes
+    $content = truncateHtml($content, $length);
 
-    $output = preg_match_all('/<h3>(.*?)<\/h3>/', $post->post_content, $matches);
-    $team_member_role = $matches[1][0];
-
-    return $team_member_role;
+    return $content;
 }
